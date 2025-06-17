33e774df8f02b5a77204bb9a925dc51c5c7aadcb
dabest822 <120066161+dabest822@users.noreply.github.com>
2025-06-17 06:54:03 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftNES",
+    defaultLocalization: "en",
     platforms: [ .iOS(.v14) ],
     products: [
         .library(name: "SwiftNES", targets: ["SwiftNES"])
