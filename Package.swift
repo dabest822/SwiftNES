e10cb72d9415e37222334e1a7d63ffc7c0d5b7f8
dabest822 <120066161+dabest822@users.noreply.github.com>
2025-06-17 06:47:36 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,16 +1,17 @@
 // swift-tools-version:5.9
 import PackageDescription
+
 let package = Package(
-  name: "SwiftNES",
-  platforms: [.iOS(.v14)],
-  products: [
-    .library(name: "SwiftNES", targets: ["SwiftNES"])
-  ],
-  targets: [
-    .target(
-      name: "SwiftNES",
-      path: "SwiftNES-Core",    // folder with the sources
-      exclude: ["Info.plist"]   // if one sits there
-    )
-  ]
+    name: "SwiftNES",
+    platforms: [ .iOS(.v14) ],
+    products: [
+        .library(name: "SwiftNES", targets: ["SwiftNES"])
+    ],
+    targets: [
+        .target(
+            name: "SwiftNES",
+            path: "SwiftNES",          // ← this folder **does** exist
+            exclude: ["Info.plist"]    // (only if there is one here)
+        )
+    ]
 )
